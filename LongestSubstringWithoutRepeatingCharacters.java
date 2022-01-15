e71a5a1c71486ba09ffd43ff65f4702386f0d581
nilezhou <22871947+NileZhou@users.noreply.github.com>
2022-01-15 14:02:04 +0800

    最长无重复子串

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,18 @@
+// https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length, ans = 0;
+        if (n < 2) return n;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i=0, j=0; i<n; ++i) {
+            if (map.containsKey(arr[i])) {
+                j = Math.max(j, map.get(arr[i]) + 1);
+            }
+            map.put(arr[i], i);
+            ans = Math.max(i - j + 1, ans);
+        }
+
+        return ans;
+    }
+}
